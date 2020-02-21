d9f9ccfe4046a1814f0963a34f42257481208625
Mateo Gonzalez <matteu20@hotmail.com>
2020-02-21 11:13:21 -0500

    cliente

---
+++ src/main/java/edu/eci/arep/laboratorio5/URLReader.java
@@ -0,0 +1,24 @@
+
+package edu.eci.arep.laboratorio5;
+
+import java.io.*; 
+import java.net.*; 
+
+/**
+ *
+ * @author david.gonzalez-g
+ */
+public class URLReader { 
+  public static void main(String[] args) throws Exception { 
+      URL url = new URL(args[0]); 
+      try (BufferedReader reader = new BufferedReader(
+          new InputStreamReader(url.openStream()))) { 
+            String inputLine = null; 
+            while ((inputLine = reader.readLine()) != null) { 
+                  System.out.println(inputLine); 
+             } 
+       } catch (IOException x) { 
+               System.err.println(x); 
+       } 
+    } 
+}
